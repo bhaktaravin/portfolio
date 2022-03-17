544356a0ea4f3829e017b2922bdf2497c335ca21
bhaktaravin <ravin.bhakta@gmail.com>
2022-03-17 13:11:25 -0700

    updated to reflect png instead of jpeg extension

--- src/components/constants/images.jsx
+++ src/components/constants/images.jsx
@@ -1,13 +1,13 @@
 import aws from '../../assests/aws.png';
 import css3 from '../../assests/css3.png';
 import html5 from '../../assests/html5.png';
-import sapcloudplatform from '../../assests/sapcloudplatform.jpeg';
+import sapcloudplatform from '../../assests/sapcloudplatform.png';
 import reactjs from '../../assests/reactjs.png';
-import profile from '../../assests/profile.jpeg';
+import profile from '../../assests/profile.png';
 import mongodb from '../../assests/mongodb.png';
 import java from '../../assests/java.png';
 import mysql from '../../assests/mysql.png';
-import spring from '../../assests/springboot.jpeg';
+import spring from '../../assests/springboot.png';
 export default {
     aws,
     css3,
